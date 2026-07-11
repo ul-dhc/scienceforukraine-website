b56d527e8fdadc99caacbd4691b4de1b61c73845
sanitareins <sanita.reinsone@gmail.com>
2026-07-12 02:58:28 +0300

    Update programmes.js

--- src/assets/js/programmes.js
+++ src/assets/js/programmes.js
@@ -460,7 +460,7 @@
     return '' +
       '<div class="share-row">' +
         '<span class="share-row__label">Share this programme</span>' +
-        '<a class="share-row__link" href="mailto:?subject=' + subject + '&body=' + body + '">' +
+        '<a class="share-row__link" href="mailto:?subject=' + subject + '&amp;body=' + body + '">' +
           '<svg class="icon" viewBox="0 0 24 24" aria-hidden="true"><path d="M4 4h16a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2z"/><polyline points="22 6 12 13 2 6"/></svg>' +
           '<span>Email</span>' +
         '</a>' +
